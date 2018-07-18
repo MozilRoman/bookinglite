91f9e97c07062912c5144a4c6ba51fbb7ecebaae
zjarex <zhunkivskyi.i@gmail.com>
2018-07-18 16:00:45 +0300

    Start Spring Boot

---
+++ src/main/java/com/softserve/edu/bookinglite/BookingliteApplication.java
@@ -0,0 +1,12 @@
+package com.softserve.edu.bookinglite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingliteApplication.class, args);
+	}
+}
