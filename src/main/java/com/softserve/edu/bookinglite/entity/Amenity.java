08ef9698955e4b9f28b5927dd6782ebce6f957f1
AlinaHrypa <alina.hrypa@gmail.com>
2018-07-26 16:53:47 +0300

    Entities

---
+++ src/main/java/com/softserve/edu/bookinglite/entity/Amenity.java
@@ -0,0 +1,53 @@
+package com.softserve.edu.bookinglite.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Entity
+@Table(name = "amenities")
+public class Amenity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @ManyToMany(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "apartment_amenities",
+            joinColumns = @JoinColumn(name = "amenity_id"),
+            inverseJoinColumns = @JoinColumn(name = "apartment_id"))
+    private Set<Apartment> apartments = new HashSet<>();
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Apartment> getApartments() {
+        return apartments;
+    }
+
+    public void setApartments(Set<Apartment> apartments) {
+        this.apartments = apartments;
+    }
+
+}
---
+++ src/main/java/com/softserve/edu/bookinglite/entity/Apartment.java
@@ -0,0 +1,95 @@
+package com.softserve.edu.bookinglite.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.math.BigDecimal;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "apartments")
+public class Apartment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    @Column(columnDefinition= "DECIMAL(8,2)")
+    private BigDecimal price;
+
+    @Column(name= "number_of_guests")
+    private int numberOfGuests;
+
+    @ManyToOne(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinColumn(name = "apartment_type_id")
+    private ApartmentType apartmentType;
+
+    @ManyToMany(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH})
+    @JoinTable(name = "apartment_amenities",
+            joinColumns = @JoinColumn(name = "apartment_id"),
+            inverseJoinColumns = @JoinColumn(name = "amenity_id"))
+    private Set<Amenity> amenities = new HashSet<>();
+
+//    @ManyToOne(cascade = {
+//			CascadeType.DETACH, CascadeType.MERGE,
+//			CascadeType.PERSIST, CascadeType.REFRESH})
+//    @JoinColumn(name = "property_id")
+//    private Property property;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public int getNumberOfGuests() {
+        return numberOfGuests;
+    }
+
+    public void setNumberOfGuests(int numberOfGuests) {
+        this.numberOfGuests = numberOfGuests;
+    }
+
+    public ApartmentType getApartmentType() {
+        return apartmentType;
+    }
+
+    public void setApartmentType(ApartmentType apartmentType) {
+        this.apartmentType = apartmentType;
+    }
+
+    public Set<Amenity> getAmenities() {
+        return amenities;
+    }
+
+    public void setAmenities(Set<Amenity> amenities) {
+        this.amenities = amenities;
+    }
+
+}
