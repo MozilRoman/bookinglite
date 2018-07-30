ab5620510a5f1dbc2430a8b92fed9967486165f3
MarianMazurak <mazurak.marian.ukr@gmail.com>
2018-07-30 14:41:12 +0300

    Created repository for all entity

---
+++ src/main/java/com/softserve/edu/bookinglite/repository/BookingStatusRepository.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.bookinglite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.softserve.edu.bookinglite.entity.BookingStatus;
+@Repository
+public interface BookingStatusRepository extends JpaRepository<BookingStatus, Long>{
+
+}
---
+++ src/main/java/com/softserve/edu/bookinglite/repository/CityRepository.java
@@ -0,0 +1,10 @@
+package com.softserve.edu.bookinglite.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.softserve.edu.bookinglite.entity.City;
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+
+}
