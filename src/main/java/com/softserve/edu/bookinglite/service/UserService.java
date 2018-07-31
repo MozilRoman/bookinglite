349a0db1e2fe2f7a47fc166d71dd6934719842ed
zjarex <zhunkivskyi.i@gmail.com>
2018-07-31 16:45:01 +0300

    fixAuth

--- src/main/java/com/softserve/edu/bookinglite/service/UserService.java
+++ src/main/java/com/softserve/edu/bookinglite/service/UserService.java
@@ -4,6 +4,7 @@ import com.softserve.edu.bookinglite.dto.RegisterDto;
 import com.softserve.edu.bookinglite.entity.Role;
 import com.softserve.edu.bookinglite.entity.User;
 import com.softserve.edu.bookinglite.mapper.UserMapper;
+import com.softserve.edu.bookinglite.repository.AddressRepository;
 import com.softserve.edu.bookinglite.repository.RoleRepository;
 import com.softserve.edu.bookinglite.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +14,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -24,6 +26,8 @@ public class UserService implements UserDetailsService {
     @Autowired
     RoleRepository roleRepository;
     @Autowired
+    AddressRepository addressRepository;
+    @Autowired
     PasswordEncoder passwordEncoder;
 
 
@@ -33,6 +37,7 @@ public class UserService implements UserDetailsService {
 
 
     //TODO: REFACTOR
+    @Transactional
     public boolean registerUser(RegisterDto registerDto){
         User user = new User();
         user.setEmail(registerDto.getEmail());
@@ -40,6 +45,7 @@ public class UserService implements UserDetailsService {
         user.setLast_name(registerDto.getLast_name());
         user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
         user.setPhone_number(registerDto.getPhone_number());
+        user.setAddress(registerDto.getAddress());
         List<Role> roles = roleRepository.findAll();
         Set<Role> userRoles = new HashSet<Role>();
         userRoles.add(roles.get(0));
