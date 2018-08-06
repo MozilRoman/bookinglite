ef2c067ec6301c61405c749ebb3e36f8f09014d6
zjarex <zhunkivskyi.i@gmail.com>
2018-08-06 22:32:06 +0300

    clear main

--- src/main/java/com/softserve/edu/bookinglite/BookingliteApplication.java
+++ src/main/java/com/softserve/edu/bookinglite/BookingliteApplication.java
@@ -1,33 +1,15 @@
 package com.softserve.edu.bookinglite;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.softserve.edu.bookinglite.repository.UserRepository;
-
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.softserve.edu.bookinglite.config.CloudinaryConfig.CloudinaryData;
 import com.softserve.edu.bookinglite.security.JwtAuthorizationFilter;
-import com.softserve.edu.bookinglite.service.ApartmentService;
-import com.softserve.edu.bookinglite.service.BookingService;
-import com.softserve.edu.bookinglite.service.dto.BookingDto;
-import com.softserve.edu.bookinglite.util.DateUtil;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.event.ApplicationEventMulticaster;
-import org.springframework.context.event.SimpleApplicationEventMulticaster;
-import org.springframework.core.task.SimpleAsyncTaskExecutor;
-import org.springframework.core.task.TaskExecutor;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
-import javax.annotation.PostConstruct;
-import java.math.BigDecimal;
-import java.util.Date;
 
 @SpringBootApplication
 public class BookingliteApplication {
@@ -48,23 +30,7 @@ public class BookingliteApplication {
 				CloudinaryData.API_KEY.getName(), CloudinaryData.API_KEY.getValue(),
 				CloudinaryData.API_SECRET.getName(), CloudinaryData.API_SECRET.getValue()));
 	}
-	/*@Autowired
-	private BookingService bookingService;
 
-	@PostConstruct
-	public void init(){
-		System.out.println(bookingService.getAllBookingsDtoByOwnerId(1l).size());
-		ObjectMapper mapper = new ObjectMapper();
-		BookingDto obj = bookingService.getAllBookingsDtoByOwnerId(1l).get(0);
-
-		try {
-			System.out.println("--------------------------------------------------------------");
-			String jsonInString = mapper.writeValueAsString(obj);
-			System.out.println(obj);
-		} catch (JsonProcessingException e) {
-			System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&7 "+e);
-		}
-	}*/
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookingliteApplication.class, args);
