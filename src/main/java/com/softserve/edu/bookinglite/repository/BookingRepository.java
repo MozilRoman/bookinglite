a2e2d57f35e4dca50faa6cce47ea150b8d24383e
lembergIP <pasternakivan@gmail.com>
2018-08-07 13:28:02 +0300

    fix repository and save booking

--- src/main/java/com/softserve/edu/bookinglite/repository/BookingRepository.java
+++ src/main/java/com/softserve/edu/bookinglite/repository/BookingRepository.java
@@ -16,13 +16,13 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 	@Query("Select b from Booking b where user.id= ?1 ORDER BY b.check_in desc")
     List<Booking> getAllByUserIdOrderByCheck_inAsc(Long id_user);
 
-    @Query("select b.apartment from Booking b where b.apartment.id=?1 and b.bookingstatus.id<3 and b.check_in <= ?2 and b.check_out>= ?2 ")
-    Apartment getBookingByCheck(Long apartment_id, Date checkDate);
-    @Query("select b.apartment from Booking b where b.apartment.id=?1 and b.bookingstatus.id<3 and b.check_in  between ?2 and ?3 ")
-    Apartment INcheckBookingsExistsByDateInAndDateOut(Long apartment_id, Date in,Date out);
+    @Query("select b.apartment from Booking b where b.apartment.id=?1 and b.bookingstatus.id<3 and b.check_in <= ?2 and b.check_out>= ?2 or  b.check_in <= ?3 and b.check_out>= ?3 ")
+    Apartment getBookingByCheck(Long apartment_id, Date in,Date out);
+    @Query("select b.apartment from Booking b where b.apartment.id=?1 and b.bookingstatus.id<3 and b.check_in  between ?2 and ?3 " +
+            "or b.bookingstatus.id<3 and b.check_out  between ?2 and ?3")
+    Apartment checkBookingsExistsByDateInAndDateOut(Long apartment_id, Date in,Date out);
 
-    @Query("select b.apartment from Booking b where b.apartment.id=?1 and b.bookingstatus.id<3 and b.check_out  between ?2 and ?3 ")
-    Apartment OUTcheckBookingsExistsByDateInAndDateOut(Long apartment_id, Date in,Date out);
+
 
 @Query("SELECT b FROM Booking b " +
         "join Apartment a on a.id=b.apartment.id " +
