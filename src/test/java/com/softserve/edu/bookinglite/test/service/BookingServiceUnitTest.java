c451cac849c2df1e4256b5b17f4c372bd388187b
Roman Mozil <Mozil_r@ukr.net>
2018-08-21 11:24:26 +0300

    test part 2

--- src/test/java/com/softserve/edu/bookinglite/test/service/BookingServiceUnitTest.java
+++ src/test/java/com/softserve/edu/bookinglite/test/service/BookingServiceUnitTest.java
@@ -3,11 +3,12 @@ package com.softserve.edu.bookinglite.test.service;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Optional;
 import java.util.Set;
 
 import org.junit.Test;
@@ -32,6 +33,7 @@ import com.softserve.edu.bookinglite.entity.User;
 import com.softserve.edu.bookinglite.exception.BookingCancelException;
 import com.softserve.edu.bookinglite.exception.BookingNotFoundException;
 import com.softserve.edu.bookinglite.repository.BookingRepository;
+import com.softserve.edu.bookinglite.repository.BookingStatusRepository;
 import com.softserve.edu.bookinglite.service.BookingService;
 import com.softserve.edu.bookinglite.service.dto.BookingDto;
 import com.softserve.edu.bookinglite.service.mapper.BookingMapper;
@@ -42,16 +44,19 @@ public class BookingServiceUnitTest {
 	
 	private static final int INDEX = 0;
 	private static final Long ID = 1L;
+	private final String RESERVED = "Reserved";
+	private final String CANCELED = "Canceled";
 	
 	@Autowired
 	private BookingService bookingService;
 	@MockBean
 	private BookingRepository bookingRepository;
+	@MockBean
+	private BookingStatusRepository bookingStatusRepository;
 	
 	@Test
 	public void findBookinDTOById() {
 		Booking bookingOptional = getBookingInstance();
-		System.out.println(bookingOptional.getCheckIn());
 		BookingDto actualBookingDto = BookingMapper.instance.bookingToBaseBookingDto(bookingOptional);
 
 		Mockito.when(bookingRepository.findBookingById(ID, ID)).thenReturn(bookingOptional);
@@ -76,24 +81,59 @@ public class BookingServiceUnitTest {
 		assertThat(actualBookingDto).isEqualTo(expectedBookingDto.get(INDEX));
 	}
 	
-	/*@Test
-	public void cancelBookingTest() throws BookingNotFoundException, BookingCancelException{
+	@Test
+	public void cancelBookingTest() throws BookingNotFoundException, BookingCancelException{//all good
 		// Arrange
 		Booking booking = getBookingInstance();
-		BookingStatus bookingStatus= booking.getBookingStatus();
-		bookingStatus.setName("Canceled");
-		booking.setBookingStatus(bookingStatus);
-		Booking actualBookingDto= booking;
-		//Booking actualBookingDto = BookingMapper.instance.bookingToBaseBookingDto(booking);
 
-		Mockito.when(bookingRepository.findBookingById(ID, ID)).thenReturn(actualBookingDto);
-		Mockito.when(bookingRepository.save(ID, ID)).thenReturn(actualBookingDto);
-		// Act
-		Booking expectedBooking = bookingService.cancelBooking(ID, ID);
-		// Assert
-		assertThat(actualBookingDto).isEqualTo(expectedBookingDto.get(INDEX));
-	}*/
+		Mockito.when(bookingRepository.findBookingById(ID, ID)).thenReturn(booking);
+		
+		BookingStatus bookingStatus= new BookingStatus();
+		bookingStatus.setId(3L);
+		bookingStatus.setName(CANCELED);
+		Mockito.when(bookingStatusRepository.findByName(CANCELED)).thenReturn(bookingStatus);
+				
+		Booking actualBooking = getBookingInstance();
+		actualBooking.setBookingStatus(bookingStatus);
+		
+		Mockito.when(bookingRepository.save(booking)).thenReturn(actualBooking);
+		boolean expectedCanceledBooking = bookingService.cancelBooking(ID, ID);		
+		
+		assertThat(expectedCanceledBooking);
+	}
 	
+	@Test(expected = BookingNotFoundException.class)//work
+	public void cancelBookingNotFoundExceptionTest() throws BookingNotFoundException, BookingCancelException{
+		Mockito.when(bookingRepository.findBookingById(ID, ID)).thenReturn(null);
+		boolean expectedBooking= bookingService.cancelBooking(ID, ID);
+		assertThat(expectedBooking);
+	}
+	
+	@Test(expected = BookingCancelException.class)///work
+	public void cancelBookingWrongBookingStatusTest() throws BookingNotFoundException, BookingCancelException{
+		// Arrange
+		Booking booking = getBookingInstance();
+		BookingStatus bookingStatus= new BookingStatus();
+		bookingStatus.setId(3L);
+		bookingStatus.setName(CANCELED);
+		booking.setBookingStatus(bookingStatus);
+		Mockito.when(bookingRepository.findBookingById(ID, ID)).thenReturn(booking);				
+		boolean expectedCanceledBooking = bookingService.cancelBooking(ID, ID);		
+		
+		assertThat(expectedCanceledBooking);
+	}
+	
+	@Test(expected = BookingCancelException.class)
+	public void cancelBookingWrongDateTest() throws BookingNotFoundException, BookingCancelException{//all good
+		// Arrange
+		Booking booking = getBookingInstance();
+		booking.setCheckIn(setAllDate("2018-01-11-14-00-00"));
+		booking.setCheckOut(setAllDate("2018-01-12-12-00-00"));
+		Mockito.when(bookingRepository.findBookingById(ID, ID)).thenReturn(booking);
+		
+		boolean expectedCanceledBooking = bookingService.cancelBooking(ID, ID);				
+		assertThat(expectedCanceledBooking);
+	}
 	
 	
 	
@@ -125,6 +165,7 @@ public class BookingServiceUnitTest {
 		role.setName("Owner");
 		// User
 		User user = new User();
+		user.setId(1L);
 		user.setFirst_name("ivan");
 		user.setLast_name("zhun");
 		user.setPassword("123");
@@ -162,13 +203,28 @@ public class BookingServiceUnitTest {
 		
 		//Booking
 		Booking booking= new Booking();
-		booking.setCheckIn(new Date(2018-11-11-14-00-00));
-		booking.setCheckOut(new Date(2018-11-15-12-00-00));
+		booking.setId(1L);
+		Date in = setAllDate("2018-11-11-14-00-00");
+		Date out = setAllDate("2018-11-15-12-00-00");
+		out.setHours(12);
+		booking.setCheckIn(in);
+		booking.setCheckOut(out);
 		booking.setTotalPrice(new BigDecimal(100));
 		booking.setApartment(apartment);
 		booking.setBookingStatus(bookingStatus);
 		booking.setUser(user);		
 		return booking;
 }
+	
+	public Date setAllDate(String stringDate) {
+		Date date=new Date();
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss");
+		 try {
+			 date= sdf.parse(stringDate);
+	      } catch (ParseException e) { 
+	         System.out.println("Unparseable using " + sdf); 
+	      }	
+		return  date;
+	}
 
 }
